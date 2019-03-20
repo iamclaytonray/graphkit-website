e579dbc3447a239eb9de7cfda9db1fb2150daaf8
Clayton Ray <iamclaytonray@gmail.com>
2019-03-20 19:39:47 -0400

    feat: add react-icons & update sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
+import { FaTerminal, FaCode } from 'react-icons/fa';
 
 import './sidebar.scss';
 
@@ -28,43 +29,25 @@ export const Sidebar = () => {
           key="sub1"
           title={
             <span>
-              <Icon type="user" />
-              subnav 1
+              <FaTerminal className="anticon" />
+              CLI
             </span>
           }
         >
-          <Menu.Item key="1">option1</Menu.Item>
-          <Menu.Item key="2">option2</Menu.Item>
-          <Menu.Item key="3">option3</Menu.Item>
-          <Menu.Item key="4">option4</Menu.Item>
+          <Menu.Item key="1">Commands</Menu.Item>
         </SubMenu>
         <SubMenu
           key="sub2"
           title={
             <span>
-              <Icon type="laptop" />
-              subnav 2
+              <FaCode className="anticon" />
+              Codegen
             </span>
           }
         >
-          <Menu.Item key="5">option5</Menu.Item>
-          <Menu.Item key="6">option6</Menu.Item>
-          <Menu.Item key="7">option7</Menu.Item>
-          <Menu.Item key="8">option8</Menu.Item>
-        </SubMenu>
-        <SubMenu
-          key="sub3"
-          title={
-            <span>
-              <Icon type="notification" />
-              subnav 3
-            </span>
-          }
-        >
-          <Menu.Item key="9">option9</Menu.Item>
-          <Menu.Item key="10">option10</Menu.Item>
-          <Menu.Item key="11">option11</Menu.Item>
-          <Menu.Item key="12">option12</Menu.Item>
+          <Menu.Item key="5">Introduction</Menu.Item>
+          <Menu.Item key="6">Getting Started</Menu.Item>
+          <Menu.Item key="7">Plugins</Menu.Item>
         </SubMenu>
       </Menu>
     </Sider>
